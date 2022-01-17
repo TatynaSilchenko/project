a689c3bb66569ed414d8030010e4f784a11db6ee
Tatyana <tatiana.s@abiatec.com>
2022-01-17 12:06:08 +0300

    work with prompt. Movies console

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
-let number = 5;
-const leftWidth = 1;
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', 0);
 
-number = 10;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
 
-console.log(number);
+const lastWatchedMovie = () => {
+    return prompt('Один из последних просмотренных фильмов?', 0);
+}
+
+const nameLastWatchedMovie = () => {
+    return prompt('Один из последних просмотренных фильмов?', 'Название');
+}
+
+const ratingLastWatchedMovie = () => {
+    return prompt('На сколько его оцените?', 0);
+}
+
+
+const allInformationAboutWatchingFilm = () => {
+    let name = nameLastWatchedMovie();
+    let rating = ratingLastWatchedMovie();
+    personalMovieDB.movies[name] = rating;
+}
+
+allInformationAboutWatchingFilm();
+allInformationAboutWatchingFilm();
+
+console.log(personalMovieDB);
 
