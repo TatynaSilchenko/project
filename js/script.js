aeca317e54f226f0a342875058b214bdd43319ae
Tatyana <tatiana.s@abiatec.com>
2022-01-17 14:50:00 +0300

    Add conditions and cycle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', 0);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0);
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,27 +9,61 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastWatchedMovie = () => {
-    return prompt('Один из последних просмотренных фильмов?', 0);
+
+// const allInformationAboutWatchingFilm = () => {
+//     let name = prompt('Один из последних просмотренных фильмов?', '');
+
+//     let rating = prompt('На сколько его оцените?');
+//     personalMovieDB.movies[name] = rating;
+// }
+
+
+let answer = 0;
+
+// while (answer < 2) {
+//     let name = nameLastWatchedMovie();
+
+//     if (name === null || name.length === 0 || name.length > 50) {
+//         continue;
+//     }
+
+//     let rating = ratingLastWatchedMovie();
+
+//     if (rating === null || rating.length === 0 || rating.length > 50) {
+//         continue;
+//     }
+//     personalMovieDB.movies[name] = rating;
+//     answer++;
+// }
+
+for (let i = 0; i < 2; i++) {
+    let name = prompt('Один из последних просмотренных фильмов?', '');
+
+    let rating = prompt('На сколько его оцените?');
+
+    if (name != null && rating != null && name != '' && rating != '' && name.length < 50 && rating.length < 50) {
+        personalMovieDB.movies[name] = rating;
+        console.log('done')
+    } else {
+        console.log('error')
+        i--;
+    }
+
+
+
 }
 
-const nameLastWatchedMovie = () => {
-    return prompt('Один из последних просмотренных фильмов?', 'Название');
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
 }
 
-const ratingLastWatchedMovie = () => {
-    return prompt('На сколько его оцените?', 0);
-}
 
-
-const allInformationAboutWatchingFilm = () => {
-    let name = nameLastWatchedMovie();
-    let rating = ratingLastWatchedMovie();
-    personalMovieDB.movies[name] = rating;
-}
-
-allInformationAboutWatchingFilm();
-allInformationAboutWatchingFilm();
 
 console.log(personalMovieDB);
 
